fcbc57fd6146e6b76b5d2222433d075c94379dba
ekkymulia <ekky.mulialasardi@gmail.com>
2023-10-08 14:11:40 +0700

    add templating sample

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Public\IndexController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('/')->group(function () {
+    
+    Route::get('/', [IndexController::class, 'index']);
+
 });
